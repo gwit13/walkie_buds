ce32b24a776a77d0ee36b353d9e93cb0804d3777
gwit13 <gwit0513@gmail.com>
2021-10-20 09:55:40 -0400

    Implement shell switchboard code

--- walkie_buds/app/src/main/java/com/gabriel/walkie_buds/MainActivity.java
+++ walkie_buds/app/src/main/java/com/gabriel/walkie_buds/MainActivity.java
@@ -1,11 +1,103 @@
 package com.gabriel.walkie_buds;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = "switchboard";
+
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+    private static String fileName = null;
+
+    private MediaRecorder recorder = null;
+    private MediaPlayer player = null;
+
+    //request permission
+    private boolean permissionToRecord = false;
+    private String[] permissions = {Manifest.permission.RECORD_AUDIO};
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case REQUEST_RECORD_AUDIO_PERMISSION:
+                permissionToRecord = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+        if(!permissionToRecord)
+            finish();
+    }
+
+    //call methods
+    private void onRecord(boolean start) {
+        if(start){
+            startRecord();
+        }
+        else{
+            stopRecord();
+        }
+    }
+
+    private void onPlay(boolean start) {
+        if(start){
+            startPlay();
+        }
+        else{
+            stopPlay();
+        }
+    }
+
+    //actionable methods
+    private void startRecord(){
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.VOICE_CALL); //change this to try different inputs
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        recorder.setOutputFile(fileName);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try{
+            recorder.prepare();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() on record failed");
+        }
+
+        recorder.start();
+    }
+
+    private void stopRecord(){
+        recorder.stop();
+        recorder.release();
+        recorder = null;
+    }
+
+    private void startPlay(){
+        player = new MediaPlayer();
+        try{
+            player.setDataSource(fileName);
+            player.prepare();
+            player.start();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() on play failed");
+        }
+    }
+
+    private void stopPlay(){
+        player.release();
+        player = null;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +106,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void shareAudio(View view) {
+
+    }
+
+    public void playback(View view) {
 
     }
 }
